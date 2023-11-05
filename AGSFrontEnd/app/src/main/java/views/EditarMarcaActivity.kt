8b47a9aca7c61ff63c988dd5345979a944aba411
Zadok <105739339+LotusZadok@users.noreply.github.com>
2023-11-05 02:13:41 -0600

    Ventanas sin funcionalidad de marcas

---
+++ AGSFrontEnd/app/src/main/java/views/EditarMarcaActivity.kt
@@ -0,0 +1,12 @@
+package views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hytan.agserviciosv1.R
+
+class EditarMarcaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_marca)
+    }
+}
---
+++ AGSFrontEnd/app/src/main/java/views/EliminarMarcaActivity.kt
@@ -0,0 +1,12 @@
+package views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hytan.agserviciosv1.R
+
+class EliminarMarcaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eliminar_marca)
+    }
+}
