beff5c146cce9bf87ef388127f21921bd81fa696
Zadok <105739339+LotusZadok@users.noreply.github.com>
2023-11-08 01:06:12 -0600

    Ya se valida número correctamente en editar cliente.

--- AGSFrontEnd/app/src/main/java/database/userDatabase.kt
+++ AGSFrontEnd/app/src/main/java/database/userDatabase.kt
@@ -13,7 +13,7 @@ class userDatabase {
 
         // Especifica la URL de tu API
         val apiUrl =
-            "https://66d1-186-177-184-229.ngrok-free.app/$endpoint"  // Reemplaza con la URL real de tu API
+            "https://aad2-152-231-128-165.ngrok.io/$endpoint"  // Reemplaza con la URL real de tu API
 
         val requestBody = json.toRequestBody("application/json".toMediaType())
 
